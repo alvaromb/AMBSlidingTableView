3651bbb2be3cfcbf1ee7993293a096e77e93fc48
Álvaro <alvaro@mbp.home>
2013-12-11 20:08:49 +0100

    Initial Commit

---
+++ AMBSlidingTableView/AMBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AMBAppDelegate.h
+//  AMBSlidingTableView
+//
+//  Created by Álvaro on 11/12/13.
+//  Copyright (c) 2013 Álvaro Medina Ballester. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AMBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AMBSlidingTableView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AMBSlidingTableView
+//
+//  Created by Álvaro on 11/12/13.
+//  Copyright (c) 2013 Álvaro Medina Ballester. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AMBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AMBAppDelegate class]));
+    }
+}
